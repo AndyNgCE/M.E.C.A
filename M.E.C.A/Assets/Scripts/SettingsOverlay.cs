32d13073e735bcbde3e4981931ddf3fe13925ec7
Mason Benell <masonbenell@knights.ucf.edu>
2021-11-27 18:27:29 -0500

    Fixed Settings
    Settings are fixed

--- M.E.C.A/Assets/Scripts/SettingsOverlay.cs
+++ M.E.C.A/Assets/Scripts/SettingsOverlay.cs
@@ -4,19 +4,48 @@ using UnityEngine;
 
 public class SettingsOverlay : MonoBehaviour
 {
-    public GameObject volumeSlider;
-    public GameObject musicSlider;
+    //public GameObject volumeSlider;
+    //public GameObject musicSlider;
+    // Defeat overlay
+    public GameObject restartLevel;
+    public GameObject quitLevel;
+    public GameObject menuFade;
+    public GameObject gameMenu;
+    public GameObject settingsButton;
+    public int menuCheck = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        volumeSlider.SetActive(false);
-        musicSlider.SetActive(false);
+        restartLevel.SetActive(false);
+        quitLevel.SetActive(false);
+        menuFade.SetActive(false);
+        gameMenu.SetActive(false);
+        settingsButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("escape"))
+        {
+            menuCheck++;
+            if(menuCheck % 2 == 1)
+            {
+                restartLevel.SetActive(true);
+                quitLevel.SetActive(true);
+                menuFade.SetActive(true);
+                gameMenu.SetActive(true);
+                settingsButton.SetActive(true);
+            }
+            else
+            {
+                restartLevel.SetActive(false);
+                quitLevel.SetActive(false);
+                menuFade.SetActive(false);
+                gameMenu.SetActive(false);
+                settingsButton.SetActive(false);
+            }
+        }
     }
 }
