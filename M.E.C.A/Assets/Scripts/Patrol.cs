4c9f58d6494f220622f81c6ab776c4e908366924
AndyNgCE <71106921+AndyNgCE@users.noreply.github.com>
2021-11-27 19:27:14 -0500

    Enemies will flip in the direction they are heading in

--- M.E.C.A/Assets/Scripts/Patrol.cs
+++ M.E.C.A/Assets/Scripts/Patrol.cs
@@ -7,21 +7,36 @@ public class Patrol : MonoBehaviour
     public float speed;
     private float waitTime;
     public float startWaitTime;
-
+    private SpriteRenderer flipper;
     public Transform[] moveSpot;
     private int randomSpot;
+    Vector2 posLastFrame;
+    Vector2 posThisFrame;
 
 
     void Start()
     {
         waitTime = startWaitTime;
         randomSpot = Random.Range(0, moveSpot.Length);
+        flipper = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, moveSpot[randomSpot].position, speed * Time.deltaTime);
+       
+        posLastFrame = posThisFrame;
+        posThisFrame = transform.position;
+
+        if (posThisFrame.x < posLastFrame.x)
+        {
+            flipper.flipX = false;
+        }
+        else if (posThisFrame.x > posLastFrame.x)
+        {
+            flipper.flipX = true;
+        }
 
         if(Vector2.Distance(transform.position, moveSpot[randomSpot].position) < 0.2f)
         {
