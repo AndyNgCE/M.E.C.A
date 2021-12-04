450de57358aad34028d328a4a7dda382284c66e8
AndyNgCE <71106921+AndyNgCE@users.noreply.github.com>
2021-12-04 13:24:15 -0500

    UPDATES

--- M.E.C.A/Assets/Scripts/GlobalControl.cs
+++ M.E.C.A/Assets/Scripts/GlobalControl.cs
@@ -2,33 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalControl
+public class GlobalControl : MonoBehaviour
 {
 
     // Base health of player starts at 300. This valuse should change between battles
     public int HP = 300;
-    /*private void Start()
-    {
-        if (PlayerPrefs.GetInt("Saved") == 1 && PlayerPrefs.GetInt("TimeToLoad") == 1)
-        {
-            HP = PlayerPrefs.GetInt("HP");
-            PlayerPrefs.Save();
-        }
-    }
-
-    public void playerHPSave()
-    {
-     //Save the current health
-    PlayerPrefs.SetInt("HP", GameController.currentHealth);
-    PlayerPrefs.SetInt("Saved", 1);
-     PlayerPrefs.Save();
-    }
-
-public void PlayerPosLoad()
-    {
-        PlayerPrefs.SetInt("TimeToLoad", 1);
-        PlayerPrefs.Save();
-    }
-    */
-    //Want to keep the variable alive through combat and menu scene swap
 }
