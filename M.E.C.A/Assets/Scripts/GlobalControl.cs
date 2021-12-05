19f22b69c9ad5dbf3bc030a3e3ae493f84ffccb1
AndyNgCE <71106921+AndyNgCE@users.noreply.github.com>
2021-12-04 22:43:54 -0500

    Update GlobalControl.cs

--- M.E.C.A/Assets/Scripts/GlobalControl.cs
+++ M.E.C.A/Assets/Scripts/GlobalControl.cs
@@ -1,10 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GlobalControl : MonoBehaviour
 {
 
     // Base health of player starts at 300. This valuse should change between battles
+    [SerializeField]
     public int HP = 300;
+    public static GlobalControl removal;
+    void Awake()
+    {
+        if (removal != null && removal != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        removal = this;
+        DontDestroyOnLoad(this);
+    }
+    void Start()
+    {
+        HP = PlayerPrefs.GetInt("health");
+    }
+
+    void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "MainMenu")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void HealthSave()
+    {
+        PlayerPrefs.SetInt("health", HP);
+    }
+
+    public int Memory()
+    {
+        return HP;
+    }
+
 }
