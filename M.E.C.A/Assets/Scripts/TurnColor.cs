7b5dd763be333fce0ae065155ebc5a784e42d040
AndyNgCE <71106921+AndyNgCE@users.noreply.github.com>
2021-12-04 22:34:37 -0500

    Color addition to combat

---
+++ M.E.C.A/Assets/Scripts/TurnColor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TurnColor : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if(currentScene.name == "CombatScene")
+            sprite.color = new Color(255, 0, 0, 1);
+        if(currentScene.name == "CombatSceneHeal")
+            sprite.color = new Color(0, 255, 0, 1);
+        if(currentScene.name == "CombatSceneBlock")
+            sprite.color = new Color(0, 0, 255, 1);
+    }
+}
